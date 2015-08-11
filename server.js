7abdd65a91a0d45ea6ad3d4a40a380be2310c97c
Jacob Pedersen <jacob@bytesizedpacket.com>
2015-08-11 12:48:34 +0200

    Improve express server

--- server.js
+++ server.js
@@ -11,14 +11,38 @@ app.engine('hbs', exphbs({
 	partialsDir: ROOT +'partials/',
 
 	extname: 'hbs',
-	defaultLayout: 'main.html'
+	defaultLayout: 'main.hbs'
 }));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-	res.render('index');
-});
-
 app.use('/static', express.static(ROOT +'static'));
+
+app.get('/*', function (req, res) {
+	var path = req.path.substring(1);
+	var file = path || 'index';
+
+	var data = {
+		thing: req.path
+	}
+
+	res.render(file, data, function(err, html) {
+		var code = 200;
+
+		if(/*hasPerms*/false) err = new Error('Unauthorized');
+
+		if(err) {
+			var msg = err.message;
+			if(msg.match(/Failed to lookup view/)) code = 404;
+			else if(msg.match(/EACCES, open/)) code = 403;
+			else if(msg.match(/Unauthorized/)) code = 401;
+			else code = 500;
+
+			res.render('error', {error: msg, code: code});
+			return;
+		}
+
+		res.send(html);
+	});
+});
 
 app.listen(3000);
